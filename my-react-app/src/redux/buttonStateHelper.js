6c3cb1a85665fc6c6f3c62703a80c70963aacd85
LeonardoFerrisi <biggestResurgence1492@gmail.com>
2025-04-11 11:01:46 -0600

    Updates

--- my-react-app/src/redux/buttonStateHelper.js
+++
@@ -1,7 +0,0 @@
-// src/redux/buttonStateHelper.js
-export const getNextButtonState = (currentState) => {
-    if (currentState === 'off') return 'solid';
-    if (currentState === 'solid') return 'flashing';
-    if (currentState === 'flashing') return 'off';
-    return 'off';
-};
